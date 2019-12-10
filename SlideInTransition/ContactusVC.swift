c87430b5915bf85b706480f3c3e3fba796376ae4
Abdul Wahab <abdul.wahab@danatonline.com>
2019-12-10 16:27:35 +0500

    first commit

---
+++ SlideInTransition/ContactusVC.swift
@@ -0,0 +1,30 @@
+//
+//  ContactusVC.swift
+//  SlideInTransition
+//
+//  Created by Abdul Wahab on 10/12/2019.
+//  Copyright © 2019 Gary Tokman. All rights reserved.
+//
+
+import UIKit
+
+class ContactusVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ SlideInTransition/HomeViewController.swift
@@ -0,0 +1,65 @@
+//
+//  ViewController.swift
+//  SlideInTransition
+//
+//  Created by Gary Tokman on 1/12/19.
+//  Copyright © 2019 Gary Tokman. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    let transiton = SlideInTransition()
+    var topView: UIView?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func didTapMenu(_ sender: UIBarButtonItem) {
+        guard let menuViewController = storyboard?.instantiateViewController(withIdentifier: "MenuViewController") as? MenuViewController else { return }
+        menuViewController.didTapMenuType = { menuType in
+            self.transitionToNew(menuType)
+        }
+        menuViewController.modalPresentationStyle = .overCurrentContext
+        menuViewController.transitioningDelegate = self
+        present(menuViewController, animated: true)
+    }
+
+    func transitionToNew(_ menuType: MenuType) {
+        let title = String(describing: menuType).capitalized
+        self.title = title
+
+        topView?.removeFromSuperview()
+        switch menuType {
+        case .profile:
+            guard let profileVC = self.storyboard?.instantiateViewController(withIdentifier: "ProfileVC") else { return }
+            view.addSubview(profileVC.view)
+            self.topView = profileVC.view
+            addChild(profileVC)
+        case .camera:
+            guard let contactUsVC = self.storyboard?.instantiateViewController(withIdentifier: "ContactusVC") else { return }
+            view.addSubview(contactUsVC.view)
+            self.topView = contactUsVC.view
+            addChild(contactUsVC)
+        default:
+            break
+        }
+    }
+
+}
+
+extension HomeViewController: UIViewControllerTransitioningDelegate {
+    func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        transiton.isPresenting = true
+        return transiton
+    }
+
+    func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        transiton.isPresenting = false
+        return transiton
+    }
+}
+
